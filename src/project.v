105c9740b035fbb9a0bea54097832f0d0f78ad82
ikkilove <yw6509@nyu.edu>
2025-02-25 18:21:21 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -3,25 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module bitwise_majority(
+    input [7:0] A,
+    input [7:0] B,
+    output reg [7:0] C
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+always @(*) begin
+    C[7] = ~(A[7] ^ B[7]);
+    C[6] = ~(A[6] ^ B[6]);
+    C[5] = ~(A[5] ^ B[5]);
+    C[4] = ~(A[4] ^ B[4]);
+    C[3] = ~(A[3] ^ B[3]);
+    C[2] = ~(A[2] ^ B[2]);
+    C[1] = ~(A[1] ^ B[1]);
+    C[0] = ~(A[0] ^ B[0]);
+end
 
 endmodule
